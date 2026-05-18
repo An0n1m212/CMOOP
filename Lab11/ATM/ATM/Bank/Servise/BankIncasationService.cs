09f5c2b1bcb0202d461310b40d8e57abe05d6747
An0n1m212 <an0n1m212afk@gmail.com>
2026-05-18 19:09:39 +0300

    pz3

---
+++ Lab11/ATM/ATM/Bank/Servise/BankIncasationService.cs
@@ -0,0 +1,25 @@
+﻿using ATM.ATM.Interface;
+using ATM.ATM;
+
+namespace ATM.Servise
+{
+    public class BankIncasationService : IAtmIncasator
+    {
+        public IAtm[] InitializeNetwork(int count, AtmCassette[] initialCash)
+        {
+            if (count <= 0) throw new ArgumentException("Кількість банкоматів має бути > 0.");
+
+            IAtm[] network = new IAtm[count];
+            for (int i = 0; i < count; i++)
+            {
+                AtmDevice atm = new AtmDevice(i + 1);
+                foreach (AtmCassette cashItem in initialCash)
+                {
+                    atm.AddBanknotes(cashItem.Denomination, cashItem.Count);
+                }
+                network[i] = atm;
+            }
+            return network;
+        }
+    }
+}
